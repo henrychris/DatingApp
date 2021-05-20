2ef2158a36f670447beb5b7e281ee8e01647872c
henrychris <henrychris2003@gmail.com>
2021-05-20 11:23:18 +0100

    end of Section 3

--- Startup.cs
+++ Startup.cs
@@ -26,11 +26,11 @@ namespace API
         }
 
         public IConfiguration Configuration { get; }
-        
+
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
-        {   
-
+        {
+            services.AddCors();
             services.AddControllers();
             services.AddDbContext<DataContext>(options =>
             {
@@ -49,6 +49,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
